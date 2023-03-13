3666ee6ea050c7bae7aadd936c19c24d139b29c9
Lorena <lorena.mndzc@gmail.com>
2023-03-13 10:47:42 +0100

    archivo sql

---
+++ consultas-multiples-tablas1.sql
@@ -0,0 +1,41 @@
+# PAIR PROGRAMMING SPRINT 2
+
+/*En esta lección de pair programming vamos a continuar trabajando sobre la base de datos Northwind.
+l día de hoy vamos a realizar ejercicios en los que practicaremos las queries SQL a múltiples 
+tablas usando los operadores CROSS JOIN, INNER JOIN y NATURAL JOIN. De esta manera podremos 
+combinar los datos de diferentes tablas en las mismas bases de datos, para así realizar 
+consultas mucho mas complejas.*/
+
+/* 1. Pedidos por empresa en UK:
+Desde las oficinas de UK nos han pedido con urgencia que realicemos una consulta a la 
+base de datos con la que podamos conocer cuántos pepidos ha realizado cada empresa cliente de UK. 
+Nos piden el ID  del cliente y el nombre de la empresa y el número de pedidos.*/
+
+/* 2.Productos pedidos por empresa en UK por año: 
+Desde Reino Unido se quedaron muy contentas con nuestra rápida respuesta a su petición anterior y 
+han decidido pedirnos una serie de consultas adicionales. La primera de ellas consiste en una query 
+que nos sirva para conocer cuántos objetos ha pedido cada empresa cliente de UK durante cada año. 
+Nos piden concretamente conocer el nombre de la empresa, el año, y la cantidad de objetos que han pedido. 
+Para ello hará falta hacer 2 joins.*/
+
+
+ /* 3.Mejorad la query anterior:
+ Lo siguiente que nos han pedido es la misma consulta anterior pero con la adición de 
+ la cantidad de dinero que han pedido por esa cantidad de objetos, teniendo en cuenta los descuentos, etc. 
+ Ojo que los descuentos en nuestra tabla nos salen en porcentajes, 
+ 15% nos sale como 0.15.*/
+
+ /* 4. BONUS: Pedidos que han realizado cada compañía y su fecha:
+ Después de estas solicitudes desde UK y graciasa a la utilidad de os resultados que se han obtenido,
+ desde la central nos han pedido una consulta que indique el nombre de cada compaía cliente 
+ junto con cada pedido que han realizado y su fecha.*/
+
+
+ /* 5. BONUS: Tipos de producto vendidos:
+ Ahora nos piden una lista con cada tipo de producto que se han vendido, sus categorías, nombre de la categoría 
+ y el nombre del producto, y el total de dinero por el que se ha vendido cada tipo de producto 
+ (teniendo en cuenta los descuentos).
+ PISTA: Necesitaréis usar 3 joints.*/
+ 
+ 
+ 
